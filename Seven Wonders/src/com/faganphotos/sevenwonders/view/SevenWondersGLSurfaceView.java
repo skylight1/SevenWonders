215892de861e4df9c87661b5643501efcd0948eb
megmeckim <timothy@nycjava.net>
2009-11-24 03:09:42 +0000

    added missing files

---
+++ Seven Wonders/src/com/faganphotos/sevenwonders/view/SevenWondersGLSurfaceView.java
@@ -0,0 +1,26 @@
+package com.faganphotos.sevenwonders.view;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
+
+public class SevenWondersGLSurfaceView extends GLSurfaceView {
+	SevenWondersGLRenderer renderer;
+
+	public SevenWondersGLSurfaceView(Context context) {
+		super(context);
+
+		setDebugFlags(DEBUG_CHECK_GL_ERROR);
+
+		renderer = new SevenWondersGLRenderer(context);
+		setRenderer(renderer);
+	}
+
+	public boolean onTouchEvent(final MotionEvent event) {
+		queueEvent(new Runnable() {
+			public void run() {
+			}
+		});
+		return true;
+	}
+}
