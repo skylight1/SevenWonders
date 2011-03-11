6c44dfba247714f1e7aa7fe57ad4136172fe746a
lnanek <lnanek@gmail.com>
2011-03-11 01:01:46 +0000

    Levels in progress, requested by Morrison. Also removes sample text from main layout that was flashing on screen before getting replaced.

--- src/skylight1/sevenwonders/ScoreActivity.java
+++ src/skylight1/sevenwonders/ScoreActivity.java
@@ -1,5 +1,6 @@
 package skylight1.sevenwonders;
 
+import skylight1.sevenwonders.levels.GameLevel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,11 +19,17 @@ public class ScoreActivity extends Activity {
 
 	static final String KEY_COLLECTED_SPELL_COUNT = "KEY_COLLECTED_SPELL_COUNT";
 
+	static final String KEY_LEVEL_ORDINAL = "KEY_LEVEL_ORDINAL";
+
+	static final String KEY_WON_LEVEL = "KEY_WON_LEVEL";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.score_activity);
 
+		final boolean wonLevel = getIntent().getBooleanExtra(KEY_WON_LEVEL, false);
+		final int level = getIntent().getIntExtra(KEY_LEVEL_ORDINAL, 0);
 		final int collectedSpellCount = getIntent().getIntExtra(
 			ScoreActivity.KEY_COLLECTED_SPELL_COUNT, 0);
 		final int yourcollectedspellcount2 = R.id.yourCollectedSpellCount;
@@ -42,6 +49,17 @@ public class ScoreActivity extends Activity {
 		final int score = collectedSpellCount * SCORE_PER_COLLECTED_SPELL 
 			+ remainingTimeSeconds * SCORE_PER_REMAINING_SECOND;
 		yourScore.setText(Integer.toString(score));
+		
+		final Button playNextLevel = (Button) findViewById(R.id.playNextLevel);
+		playNextLevel.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent().setClass(ScoreActivity.this, PlayActivity.class);
+				intent.putExtra(KEY_LEVEL_ORDINAL, level + 1);
+				startActivity(intent);
+				finish();
+			}
+		});
 
 		final Button playAgain = (Button) findViewById(R.id.playAgain);
 		playAgain.setOnClickListener(new OnClickListener() {
@@ -52,5 +70,22 @@ public class ScoreActivity extends Activity {
 				finish();
 			}
 		});
+		
+		// If the user won the level and there is a next level, show the 
+		// play next level button.
+		boolean nextLevelExists = level < GameLevel.values().length - 1;
+		if (wonLevel && nextLevelExists) {
+			playNextLevel.setVisibility(View.VISIBLE);
+			playAgain.setVisibility(View.GONE);
+		// Otherwise show the play again button.
+		} else {
+			playNextLevel.setVisibility(View.VISIBLE);
+			playAgain.setVisibility(View.GONE);
+		}
+		
+		// Tell the user they won the game if they won the last level 
+		// and there are no more levels.
+		TextView wonGame = (TextView) findViewById(R.id.wonGame);
+		wonGame.setVisibility(wonLevel && !nextLevelExists ? View.VISIBLE : View.GONE);
 	}
 }
