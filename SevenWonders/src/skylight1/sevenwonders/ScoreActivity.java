7541adbb7557e9aa0a4656a4bd96bd7e9d0ac2b4
reccardt <rob@eccardt.com>
2011-03-17 23:33:38 +0000

    Post scores to Facebook.

--- SevenWonders/src/skylight1/sevenwonders/ScoreActivity.java
+++ SevenWonders/src/skylight1/sevenwonders/ScoreActivity.java
@@ -1,5 +1,7 @@
 package skylight1.sevenwonders;
 
+import skylight1.sevenwonders.social.facebook.FacebookScoreActivity;
+import skylight1.sevenwonders.social.facebook.FacebookConfig;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +19,8 @@ public class ScoreActivity extends Activity {
 	static final String KEY_REMAINING_TIME_SECONDS = "KEY_REMAINING_TIME_SECONDS";
 
 	static final String KEY_COLLECTED_SPELL_COUNT = "KEY_COLLECTED_SPELL_COUNT";
+	
+	private String myScore;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,7 +45,8 @@ public class ScoreActivity extends Activity {
 		final TextView yourScore = (TextView) findViewById(R.id.yourScore);
 		final int score = collectedSpellCount * SCORE_PER_COLLECTED_SPELL 
 			+ remainingTimeSeconds * SCORE_PER_REMAINING_SECOND;
-		yourScore.setText(Integer.toString(score));
+		myScore = Integer.toString(score);
+		yourScore.setText(myScore);
 
 		final Button playAgain = (Button) findViewById(R.id.playAgain);
 		playAgain.setOnClickListener(new OnClickListener() {
@@ -50,6 +55,25 @@ public class ScoreActivity extends Activity {
 				Intent intent = new Intent().setClass(ScoreActivity.this, PlayActivity.class);
 				startActivity(intent);
 				finish();
+			}
+		});
+		
+		final Button postToFacebook = (Button) findViewById(R.id.postToFacebook);
+		postToFacebook.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ScoreActivity.this, FacebookScoreActivity.class);
+				// The app id used here in initFacebook is my own Facebook app. Seven Wonders will 
+				// need its own. Instead of embedding your application secret into the application (which would be hackable)
+				// Facebook uses a hash generated from your Android keystore, which you then add to your application at
+				// the Facebook site. Instructions here: https://developers.facebook.com/docs/guides/mobile/
+				// While using the hash is safer, it means that everyone who wants to debug this will have to use
+				// the same key store.
+				FacebookConfig.initFacebook("148539428520612",R.drawable.icon);
+				intent.putExtra(FacebookScoreActivity.WALL_POST_PARAMS_EXTRA_KEY, new Bundle());
+				intent.putExtra(FacebookScoreActivity.MY_SCORE, myScore);
+				startActivity(intent);
+				//finish();
 			}
 		});
 	}
