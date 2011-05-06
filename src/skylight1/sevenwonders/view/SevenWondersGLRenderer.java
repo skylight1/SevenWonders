ce8eceefeba1cdf1ed8e8ba0fb20df7c68701e56
lnanek <lnanek@gmail.com>
2011-05-06 01:28:21 +0000

    Fix screwed up rendering after going to another app and coming back after collecting an ankh. Introduces new, not as bad bug, of ankhs you collected coming back when you do so.

--- src/skylight1/sevenwonders/view/SevenWondersGLRenderer.java
+++ src/skylight1/sevenwonders/view/SevenWondersGLRenderer.java
@@ -1,7 +1,6 @@
 package skylight1.sevenwonders.view;
 
-import static javax.microedition.khronos.opengles.GL10.GL_CCW;
-import static javax.microedition.khronos.opengles.GL10.GL_CW;
+import static javax.microedition.khronos.opengles.GL10.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,8 +14,8 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import skylight1.opengl.CollisionDetector;
-import skylight1.opengl.GeometryBuilder;
 import skylight1.opengl.CollisionDetector.CollisionObserver;
+import skylight1.opengl.GeometryBuilder;
 import skylight1.opengl.GeometryBuilder.NormalizableTriangle3D;
 import skylight1.opengl.GeometryBuilder.TexturableRectangle2D;
 import skylight1.opengl.GeometryBuilder.TexturableTriangle3D;
@@ -81,17 +80,17 @@ public class SevenWondersGLRenderer implements Renderer {
 
 	private long timeAtLastOnRenderCall;
 
-	private final CollisionDetector collisionDetector = new CollisionDetector();
+	private CollisionDetector collisionDetector;
 
-	private final CollisionDetector obstacleCollisionDetector = new CollisionDetector();
+	private CollisionDetector obstacleCollisionDetector;
 	
 	private int score;
 
 	private Carpet carpet;
 
-	private List<OpenGLGeometry> decorationGeometries = new ArrayList<OpenGLGeometry>();
+	private List<OpenGLGeometry> decorationGeometries;
 	
-	private final Map<Integer, Texture> textureResourceIdToTextureMap = new HashMap<Integer, Texture>();
+	private Map<Integer, Texture> textureResourceIdToTextureMap;
 
 	private final Handler updateUiHandler;
 
@@ -107,7 +106,7 @@ public class SevenWondersGLRenderer implements Renderer {
 
 	private Integer swordAnimationIndex;
 
-	private final OpenGLGeometryBuilder<GeometryBuilder.TexturableTriangle3D<GeometryBuilder.NormalizableTriangle3D<Object>>, GeometryBuilder.TexturableRectangle2D<Object>> openGLGeometryBuilder;
+	private OpenGLGeometryBuilder<GeometryBuilder.TexturableTriangle3D<GeometryBuilder.NormalizableTriangle3D<Object>>, GeometryBuilder.TexturableRectangle2D<Object>> openGLGeometryBuilder;
 
 	private float startOfFramePlayerWorldPositionX;
 
@@ -116,125 +115,11 @@ public class SevenWondersGLRenderer implements Renderer {
 	public SevenWondersGLRenderer(final Context aContext, final Handler aUpdateUiHandler, final GameLevel aLevel) {
 		Log.i(TAG, "SevenWondersGLRenderer()");
 		context = aContext;
-		carpet = new Carpet(this);
 		level = aLevel;
 		updateUiHandler = aUpdateUiHandler;
-
-		openGLGeometryBuilder = OpenGLGeometryBuilderFactory.createTexturableNormalizable(60453);
-
-		// load all of the decorations (land, water, sphinx, pyramids, etc.)
-		int currentTextureResource = 0;
-		for (GameObjectDescriptor objectDescriptor : level.getDecorations()) {
-			// if the texture has changed (including the first time through), then ...
-			if (objectDescriptor.textureResource != currentTextureResource) {
-				// wrap up the existing geometry (if any, since first time through there won't be an existing geometry)
-				if (openGLGeometryBuilder.isBuildingGeometry()) {
-					final OpenGLGeometry previousGeometry = openGLGeometryBuilder.endGeometry();
-					decorationGeometries.add(previousGeometry);
-				}
-				
-				currentTextureResource = objectDescriptor.textureResource;
-				
-				// create a new texture object, store it in the texture id to texture map,
-				// and start a new geometry using the new texture
-				final Texture texture = getTexture(objectDescriptor.textureResource, true);
-				openGLGeometryBuilder.startGeometry(texture);
-			}
-
-			// load the object
-			TransformingGeometryBuilder<GeometryBuilder.TexturableTriangle3D<GeometryBuilder.NormalizableTriangle3D<Object>>, GeometryBuilder.TexturableRectangle2D<Object>> transformingGeometryBuilder = new TransformingGeometryBuilder<TexturableTriangle3D<NormalizableTriangle3D<Object>>, TexturableRectangle2D<Object>>(openGLGeometryBuilder, objectDescriptor.coordinateTransformationMatrix, objectDescriptor.textureTransformationMatrix);
-			loadRequiredObj(objectDescriptor.objectFileResourceId, transformingGeometryBuilder);
-		}
-		// wrap up the existing geometry (if any, since first time through there won't be an existing geometry)
-		if (openGLGeometryBuilder.isBuildingGeometry()) {
-			final OpenGLGeometry lastGeometry = openGLGeometryBuilder.endGeometry();
-			decorationGeometries.add(lastGeometry);
-		}
-		
-		openGLGeometryBuilder.startGeometry(getTexture(R.raw.skybox_texture, false));		
-		loadRequiredObj(R.raw.skybox_model, openGLGeometryBuilder);
-		skyboxGeometry = openGLGeometryBuilder.endGeometry();
-		
-		final GeometryAwareCollisionObserver spellsCollisionHandler = new SpellCollisionHandler(collisionDetector, updateUiHandler, this);
-		allSpellsGeometry = addObjectsToGeometry(openGLGeometryBuilder, spellsCollisionHandler, level.getSpells(), R.raw.textures);
-		final GeometryAwareCollisionObserver hazardCollisionObserver = new HazardCollisionHandler(updateUiHandler);
-		allHazardsGeometry = addObjectsToGeometry(openGLGeometryBuilder, hazardCollisionObserver, level.getHazards(), R.raw.textures);
-		carpet.createGeometry(openGLGeometryBuilder);
-
-		// collision with an obstacle moves the player back to where
-		// they were at the start of the frame, prior to any movement
-		final CollisionObserver obstacleCollisionObserver = new CollisionObserver() {
-			@Override
-			public void collisionOccurred(final float[] aBoundingSphere) {
-			    SoundTracks.getInstance().play(SoundTracks.BUMP);
-		
-			    // Find the distance traveled toward the pyramid.
-				float initialOffsetToPyramidX = aBoundingSphere[0] - startOfFramePlayerWorldPositionX;
-				float initialOffsetToPyramidZ = aBoundingSphere[2] - startOfFramePlayerWorldPositionZ;
-				float collidingOffsetToPyramidX = aBoundingSphere[0] - playerWorldPosition.x;
-				float collidingOffsetToPyramidZ = aBoundingSphere[2] - playerWorldPosition.z;
-				float initialDistanceFromPyramid = Matrix.length(Math.abs(initialOffsetToPyramidX), 0, Math.abs(initialOffsetToPyramidZ));
-				float collidingDistanceFromPyramid = Matrix.length(Math.abs(collidingOffsetToPyramidX), 0, Math.abs(collidingOffsetToPyramidZ));
-				float distanceTraveledTowardPyramid = initialDistanceFromPyramid - collidingDistanceFromPyramid;
-
-				// Find X offset to travel that distance away using same slope as if headed toward pyramid.
-				float xToZ = initialOffsetToPyramidX / initialOffsetToPyramidZ;
-				float slideAmountZ = FloatMath.sqrt(distanceTraveledTowardPyramid * distanceTraveledTowardPyramid / (xToZ * xToZ + 1));
-				float slideOffsetZ = slideAmountZ * initialOffsetToPyramidZ / Math.abs(initialOffsetToPyramidZ);
-
-				// Find Z offset to travel that distance away using same slope as if headed toward pyramid.
-				float zToX = initialOffsetToPyramidZ / initialOffsetToPyramidX;
-				float slideAmountX = FloatMath.sqrt(distanceTraveledTowardPyramid * distanceTraveledTowardPyramid / (zToX * zToX + 1));
-				float slideOffsetX = slideAmountX * initialOffsetToPyramidX / Math.abs(initialOffsetToPyramidX);
-				
-				// Take where the player would have gone had he not collided
-				// and subtract the distance traveled toward the pyramid. 
-				// This should leave only the motion to the side of the pyramid.
-				float playerSlidingPositionX = playerWorldPosition.x - slideOffsetX;
-				float playerSlidingPositionZ = playerWorldPosition.z - slideOffsetZ;
-				
-				playerWorldPosition.x = playerSlidingPositionX;
-				playerWorldPosition.z = playerSlidingPositionZ;
-			    
-				float slidingOffsetToPyramidX = aBoundingSphere[0] - playerWorldPosition.x;
-				float slidingOffsetToPyramidZ = aBoundingSphere[2] - playerWorldPosition.z;
-				float slidingDistanceFromPyramid = Matrix.length(Math.abs(slidingOffsetToPyramidX), 0, Math.abs(slidingOffsetToPyramidZ));
-				Log.i(TAG, String.format("just colided with (%s) at distance %f, retreating to distance %f", 
-						Arrays.toString(aBoundingSphere), collidingOffsetToPyramidZ, slidingDistanceFromPyramid));
-			}
-		};
-		for (final float[] boundingSphere : level.getObstacles()) {
-			obstacleCollisionDetector.addBoundingSphere(boundingSphere, obstacleCollisionObserver);
-		}
 	}
 
 	public void onSurfaceCreated(final GL10 aGl, final EGLConfig aConfig) {
-		Log.i(TAG, "- onSurfaceCreated - ");
-
-		openGLGeometryBuilder.enable(aGl);
-
-		aGl.glColor4f(1, 1, 1, 1);
-		aGl.glClearColor(0.5f, 0.5f, 1, 1.0f);
-
-		// Don't draw inside facing surfaces on things like the pyramid and sphinx.
-		// This fixes a z-fighting issue: At long distances OpenGL doesn't have enough precision in the depth buffer
-		// to tell if the front is closer or if the inside of a nearby back surface is closer and gets it wrong some
-		// times.
-		aGl.glEnable(GL10.GL_CULL_FACE);
-		aGl.glEnable(GL10.GL_DEPTH_TEST);
-		aGl.glDisable(GL10.GL_BLEND);
-		aGl.glBlendFunc(GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA);
-		aGl.glShadeModel(GL10.GL_SMOOTH);
-		aGl.glEnable(GL10.GL_LIGHTING);
-		aGl.glLightModelfv(GL10.GL_LIGHT_MODEL_AMBIENT, new float[] { 0.75f, 0.75f, 0.75f, 1f }, 0);
-		aGl.glEnable(GL10.GL_LIGHT0);
-		aGl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, new float[] { -1f, 0f, 1f, 0.0f }, 0);
-		aGl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, new float[] { 0.5f, 0.5f, 0.5f, 1f }, 0);
-
-		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT, new float[] { 1.0f, 1.0f, 1.0f, 1.0f }, 0);
-		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_DIFFUSE, new float[] { 1.0f, 1.0f, 1.0f, 1.0f }, 0);
-		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SPECULAR, new float[] { 0.1f, 0.1f, 0.1f, 1.0f }, 0);
-		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SHININESS, new float[] { 50.0f }, 0);
 	}
 
 	private OpenGLGeometry[] addObjectsToGeometry(
@@ -329,12 +214,134 @@ public class SevenWondersGLRenderer implements Renderer {
 	}
 
 	public void onSurfaceChanged(final GL10 aGl, final int aW, final int aH) {
+		
+
+		collisionDetector = new CollisionDetector();
+		obstacleCollisionDetector = new CollisionDetector();
+		
+		decorationGeometries = new ArrayList<OpenGLGeometry>();
+		textureResourceIdToTextureMap = new HashMap<Integer, Texture>();
+		carpet = new Carpet(this);
+
+		openGLGeometryBuilder = OpenGLGeometryBuilderFactory.createTexturableNormalizable(60453);
+
+		// load all of the decorations (land, water, sphinx, pyramids, etc.)
+		int currentTextureResource = 0;
+		for (GameObjectDescriptor objectDescriptor : level.getDecorations()) {
+			// if the texture has changed (including the first time through), then ...
+			if (objectDescriptor.textureResource != currentTextureResource) {
+				// wrap up the existing geometry (if any, since first time through there won't be an existing geometry)
+				if (openGLGeometryBuilder.isBuildingGeometry()) {
+					final OpenGLGeometry previousGeometry = openGLGeometryBuilder.endGeometry();
+					decorationGeometries.add(previousGeometry);
+				}
+				
+				currentTextureResource = objectDescriptor.textureResource;
+				
+				// create a new texture object, store it in the texture id to texture map,
+				// and start a new geometry using the new texture
+				final Texture texture = getTexture(objectDescriptor.textureResource, true);
+				openGLGeometryBuilder.startGeometry(texture);
+			}
+
+			// load the object
+			TransformingGeometryBuilder<GeometryBuilder.TexturableTriangle3D<GeometryBuilder.NormalizableTriangle3D<Object>>, GeometryBuilder.TexturableRectangle2D<Object>> transformingGeometryBuilder = new TransformingGeometryBuilder<TexturableTriangle3D<NormalizableTriangle3D<Object>>, TexturableRectangle2D<Object>>(openGLGeometryBuilder, objectDescriptor.coordinateTransformationMatrix, objectDescriptor.textureTransformationMatrix);
+			loadRequiredObj(objectDescriptor.objectFileResourceId, transformingGeometryBuilder);
+		}
+		// wrap up the existing geometry (if any, since first time through there won't be an existing geometry)
+		if (openGLGeometryBuilder.isBuildingGeometry()) {
+			final OpenGLGeometry lastGeometry = openGLGeometryBuilder.endGeometry();
+			decorationGeometries.add(lastGeometry);
+		}
+		
+		openGLGeometryBuilder.startGeometry(getTexture(R.raw.skybox_texture, false));		
+		loadRequiredObj(R.raw.skybox_model, openGLGeometryBuilder);
+		skyboxGeometry = openGLGeometryBuilder.endGeometry();
+		
+		final GeometryAwareCollisionObserver spellsCollisionHandler = new SpellCollisionHandler(collisionDetector, updateUiHandler, this);
+		allSpellsGeometry = addObjectsToGeometry(openGLGeometryBuilder, spellsCollisionHandler, level.getSpells(), R.raw.textures);
+		final GeometryAwareCollisionObserver hazardCollisionObserver = new HazardCollisionHandler(updateUiHandler);
+		allHazardsGeometry = addObjectsToGeometry(openGLGeometryBuilder, hazardCollisionObserver, level.getHazards(), R.raw.textures);
+		carpet.createGeometry(openGLGeometryBuilder);
+
+		// collision with an obstacle moves the player back to where
+		// they were at the start of the frame, prior to any movement
+		final CollisionObserver obstacleCollisionObserver = new CollisionObserver() {
+			@Override
+			public void collisionOccurred(final float[] aBoundingSphere) {
+			    SoundTracks.getInstance().play(SoundTracks.BUMP);
+		
+			    // Find the distance traveled toward the pyramid.
+				float initialOffsetToPyramidX = aBoundingSphere[0] - startOfFramePlayerWorldPositionX;
+				float initialOffsetToPyramidZ = aBoundingSphere[2] - startOfFramePlayerWorldPositionZ;
+				float collidingOffsetToPyramidX = aBoundingSphere[0] - playerWorldPosition.x;
+				float collidingOffsetToPyramidZ = aBoundingSphere[2] - playerWorldPosition.z;
+				float initialDistanceFromPyramid = Matrix.length(Math.abs(initialOffsetToPyramidX), 0, Math.abs(initialOffsetToPyramidZ));
+				float collidingDistanceFromPyramid = Matrix.length(Math.abs(collidingOffsetToPyramidX), 0, Math.abs(collidingOffsetToPyramidZ));
+				float distanceTraveledTowardPyramid = initialDistanceFromPyramid - collidingDistanceFromPyramid;
+
+				// Find X offset to travel that distance away using same slope as if headed toward pyramid.
+				float xToZ = initialOffsetToPyramidX / initialOffsetToPyramidZ;
+				float slideAmountZ = FloatMath.sqrt(distanceTraveledTowardPyramid * distanceTraveledTowardPyramid / (xToZ * xToZ + 1));
+				float slideOffsetZ = slideAmountZ * initialOffsetToPyramidZ / Math.abs(initialOffsetToPyramidZ);
+
+				// Find Z offset to travel that distance away using same slope as if headed toward pyramid.
+				float zToX = initialOffsetToPyramidZ / initialOffsetToPyramidX;
+				float slideAmountX = FloatMath.sqrt(distanceTraveledTowardPyramid * distanceTraveledTowardPyramid / (zToX * zToX + 1));
+				float slideOffsetX = slideAmountX * initialOffsetToPyramidX / Math.abs(initialOffsetToPyramidX);
+				
+				// Take where the player would have gone had he not collided
+				// and subtract the distance traveled toward the pyramid. 
+				// This should leave only the motion to the side of the pyramid.
+				float playerSlidingPositionX = playerWorldPosition.x - slideOffsetX;
+				float playerSlidingPositionZ = playerWorldPosition.z - slideOffsetZ;
+				
+				playerWorldPosition.x = playerSlidingPositionX;
+				playerWorldPosition.z = playerSlidingPositionZ;
+			    
+				float slidingOffsetToPyramidX = aBoundingSphere[0] - playerWorldPosition.x;
+				float slidingOffsetToPyramidZ = aBoundingSphere[2] - playerWorldPosition.z;
+				float slidingDistanceFromPyramid = Matrix.length(Math.abs(slidingOffsetToPyramidX), 0, Math.abs(slidingOffsetToPyramidZ));
+				Log.i(TAG, String.format("just colided with (%s) at distance %f, retreating to distance %f", 
+						Arrays.toString(aBoundingSphere), collidingOffsetToPyramidZ, slidingDistanceFromPyramid));
+			}
+		};
+		for (final float[] boundingSphere : level.getObstacles()) {
+			obstacleCollisionDetector.addBoundingSphere(boundingSphere, obstacleCollisionObserver);
+		}
+		
+		openGLGeometryBuilder.enable(aGl);
+
+		aGl.glColor4f(1, 1, 1, 1);
+		aGl.glClearColor(0.5f, 0.5f, 1, 1.0f);
+
+		// Don't draw inside facing surfaces on things like the pyramid and sphinx.
+		// This fixes a z-fighting issue: At long distances OpenGL doesn't have enough precision in the depth buffer
+		// to tell if the front is closer or if the inside of a nearby back surface is closer and gets it wrong some
+		// times.
+		aGl.glEnable(GL10.GL_CULL_FACE);
+		aGl.glEnable(GL10.GL_DEPTH_TEST);
+		aGl.glDisable(GL10.GL_BLEND);
+		aGl.glBlendFunc(GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		aGl.glShadeModel(GL10.GL_SMOOTH);
+		aGl.glEnable(GL10.GL_LIGHTING);
+		aGl.glLightModelfv(GL10.GL_LIGHT_MODEL_AMBIENT, new float[] { 0.75f, 0.75f, 0.75f, 1f }, 0);
+		aGl.glEnable(GL10.GL_LIGHT0);
+		aGl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, new float[] { -1f, 0f, 1f, 0.0f }, 0);
+		aGl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, new float[] { 0.5f, 0.5f, 0.5f, 1f }, 0);
+
+		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT, new float[] { 1.0f, 1.0f, 1.0f, 1.0f }, 0);
+		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_DIFFUSE, new float[] { 1.0f, 1.0f, 1.0f, 1.0f }, 0);
+		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SPECULAR, new float[] { 0.1f, 0.1f, 0.1f, 1.0f }, 0);
+		aGl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SHININESS, new float[] { 50.0f }, 0);
+		
 		aGl.glMatrixMode(GL10.GL_PROJECTION);
 		aGl.glLoadIdentity();
 		aGl.glViewport(0, 0, aW, aH);
 		GLU.gluPerspective(aGl, 45, (float) aW / (float) aH, 0.1f, 5000f);
 
 		aGl.glMatrixMode(GL10.GL_MODELVIEW);
+		aGl.glLoadIdentity();
 		
 		// load all of the used textures
 		for (Texture texture : textureResourceIdToTextureMap.values()) {
@@ -356,7 +363,9 @@ public class SevenWondersGLRenderer implements Renderer {
 	}
 	
 	public void onDrawFrame(final GL10 aGl) {
-		aGl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		Log.i("7WRenderer", "player position: " + playerWorldPosition);
+		
+		aGl.glClear(GL10.GL_DEPTH_BUFFER_BIT);
 		
 		playerFacingThisFrame = playerFacing;
 		
