9de28f8290df5c1c3d447d2708d441d8d69b38b3
codeledger <morrison.chang@gmail.com>
2011-06-19 20:06:22 +0000

    Added controls for Xperia Play buttons/control pad.

--- SevenWonders/src/skylight1/sevenwonders/view/SevenWondersGLSurfaceView.java
+++ SevenWonders/src/skylight1/sevenwonders/view/SevenWondersGLSurfaceView.java
@@ -4,8 +4,8 @@ import skylight1.sevenwonders.GameState;
 import skylight1.sevenwonders.levels.GameLevel;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Handler;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -17,6 +17,12 @@ public class SevenWondersGLSurfaceView extends GLSurfaceView {
 	private SevenWondersGLRenderer renderer;
 	
 	private TiltControl tiltControl;
+	
+	private int controlMode;
+	static final int CONTROL_MODE_DEFAULT = 0;
+	// 0: default accelerometer / D-Pad
+	static final int CONTROL_MODE_XPERIA_PLAY = 1;
+	// 1: Sony Ericsson Xperia Play	
 
 	private final GameState gameState;
 	
@@ -33,8 +39,13 @@ public class SevenWondersGLSurfaceView extends GLSurfaceView {
 	public void initialize() {
 		//setDebugFlags(DEBUG_CHECK_GL_ERROR);
 	
-		tiltControl = new TiltControl(getContext(), renderer);
-	
+		if(Build.MODEL.contains("R800a")) {
+			controlMode = 1;
+		} else {
+			controlMode = 0;
+			tiltControl = new TiltControl(getContext(), renderer);
+		}
+		
 		setClickable(true);
 		setFocusable(true);
 		setFocusableInTouchMode(true);
@@ -45,14 +56,16 @@ public class SevenWondersGLSurfaceView extends GLSurfaceView {
 	
 	@Override
 	public void onPause() {
-		tiltControl.stop();
+		if(controlMode == 0)
+			tiltControl.stop();
 		super.onPause();
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		tiltControl.start();
+		if(controlMode == 0)
+			tiltControl.start();
 	}
 
 	public boolean onTouchEvent(final MotionEvent aEvent) {
@@ -66,31 +79,66 @@ public class SevenWondersGLSurfaceView extends GLSurfaceView {
 
 	@Override
 	public boolean onKeyDown(final int aKeyCode, final KeyEvent aEvent) {
-		switch (aKeyCode) {
-			case KeyEvent.KEYCODE_DPAD_CENTER:
-			case KeyEvent.KEYCODE_SPACE:
-				renderer.setPlayerVelocity(0);
-				return true;
-				
-			// left/q -> left
-			case KeyEvent.KEYCODE_DPAD_LEFT:
-			case KeyEvent.KEYCODE_Q:
-				renderer.turn(-5f);
-				return true;
-				
-			// right/w -> right
-			case KeyEvent.KEYCODE_DPAD_RIGHT:
-			case KeyEvent.KEYCODE_W:
-				renderer.turn(+5f);
-				return true;
-				
-			case KeyEvent.KEYCODE_DPAD_UP:
-				renderer.changeVelocity(SevenWondersGLRenderer.MAXIMUM_VELOCITY / 10f);
-				return true;
-				
-			case KeyEvent.KEYCODE_DPAD_DOWN:
-				renderer.changeVelocity(-SevenWondersGLRenderer.MAXIMUM_VELOCITY / 10f);
-				return true;
+		
+		if(controlMode == CONTROL_MODE_XPERIA_PLAY) {
+			switch(aKeyCode) {
+				// left
+				case KeyEvent.KEYCODE_DPAD_LEFT:
+					renderer.turn(-5f);
+					return true;
+					
+				// right	
+				case KeyEvent.KEYCODE_DPAD_RIGHT:
+					renderer.turn(5f);
+					return true;
+					
+				// accelerate
+				case KeyEvent.KEYCODE_DPAD_CENTER:
+					renderer.changeVelocity(SevenWondersGLRenderer.MAXIMUM_VELOCITY / 10f);
+					return true;
+					
+				case KeyEvent.KEYCODE_BACK:
+					if(aEvent.isAltPressed()) {
+						renderer.changeVelocity(-SevenWondersGLRenderer.MAXIMUM_VELOCITY / 10f);
+						return true;
+					} else {
+						// pass regular back button event to system
+						return super.onKeyDown(aKeyCode, aEvent);
+					}
+					
+				case 99:
+				case 100:
+					// API 9 introduced KEYCODE_BUTTON_X, KEYCODE_BUTTON_Y
+					renderer.setPlayerVelocity(0);
+					return true;
+			}
+		} else {
+			switch (aKeyCode) {
+				case KeyEvent.KEYCODE_DPAD_CENTER:
+				case KeyEvent.KEYCODE_SPACE:
+					renderer.setPlayerVelocity(0);
+					return true;
+
+					// left/q -> left
+				case KeyEvent.KEYCODE_DPAD_LEFT:
+				case KeyEvent.KEYCODE_Q:
+					renderer.turn(-5f);
+					return true;
+
+					// right/w -> right
+				case KeyEvent.KEYCODE_DPAD_RIGHT:
+				case KeyEvent.KEYCODE_W:
+					renderer.turn(+5f);
+					return true;
+
+				case KeyEvent.KEYCODE_DPAD_UP:
+					renderer.changeVelocity(SevenWondersGLRenderer.MAXIMUM_VELOCITY / 10f);
+					return true;
+
+				case KeyEvent.KEYCODE_DPAD_DOWN:
+					renderer.changeVelocity(-SevenWondersGLRenderer.MAXIMUM_VELOCITY / 10f);
+					return true;
+			}
 		}
 
 		return false;
