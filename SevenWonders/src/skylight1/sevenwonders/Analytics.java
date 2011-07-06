bb384af583731c47890b04be20a18eb1c5f7f6c0
dariol <dario@nycjava.net>
2011-07-06 03:38:53 +0000

    cleaned up null ptr handling -> moved inside Analytics class,  cleaned up tracker in lifecycle methods, added beta label to version in manifest for tracking purposes

--- SevenWonders/src/skylight1/sevenwonders/Analytics.java
+++ SevenWonders/src/skylight1/sevenwonders/Analytics.java
@@ -15,47 +15,49 @@ private GoogleAnalyticsTracker tracker;
 	public Analytics(Context context) {
 		ga_id = Assets.getString("ga_id",context);
 //TODO: REFLECT!
-        tracker = GoogleAnalyticsTracker.getInstance();
-        tracker.setProductVersion("7W", BuildInfo.getVersionName(context));
-	}
-
-	private boolean isValid() {
         if(ga_id.length()>0) {
-        	return true;
+	        tracker = GoogleAnalyticsTracker.getInstance();
+	        tracker.setProductVersion("7W", BuildInfo.getVersionName(context));
         }
-        return false;
 	}
 	
-	public static Analytics getInstance(Context context, String name, String version) {
+	public static Analytics getInstance(Context context, String name, String version) {		
         Analytics analytics = new Analytics(context);
-        if(!analytics.isValid()) {
-    	    return null;
-        }
         return analytics;
     }
 
 	public void start(Context context) {
 //TODO: REFLECT! and note: start tracker can be started with a dispatch interval (in seconds) so add that method!
-        tracker.start(ga_id, context);
+		if(tracker!=null) {
+			tracker.start(ga_id, context);
+		}
     }
 
 	public void trackEvent(String string, String string2, String hashedPhoneId, int i) {
 // TODO: REFLECT!
-	    tracker.trackEvent(string, string2, hashedPhoneId, i);
+		if(tracker!=null) {
+			tracker.trackEvent(string, string2, hashedPhoneId, i);
+		}
     }
 
 	public void trackPageView(String string) {
 // TODO: REFLECT!
-	    tracker.trackPageView(string);
+		if(tracker!=null) {
+			tracker.trackPageView(string);
+		}
     }
 
 	public void dispatch() {
 // TODO: REFLECT!
-	    tracker.dispatch();
+		if(tracker!=null) {
+			tracker.dispatch();
+		}
     }
 
 	public void stop() {
 // TODO: REFLECT!
-	    tracker.stop();
-    }
+		if(tracker!=null) {
+			tracker.stop();
+		}
+	}
 }
