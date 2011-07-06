00305c63cd74bb09745e2fc08405e1004f3f04b2
lnanek <lnanek@gmail.com>
2011-07-06 00:28:50 +0000

    Different sound for coin collection.

--- src/skylight1/sevenwonders/levels/CoinCollisionAction.java
+++ src/skylight1/sevenwonders/levels/CoinCollisionAction.java
@@ -3,17 +3,14 @@
  */
 package skylight1.sevenwonders.levels;
 
-import skylight1.sevenwonders.PlayActivity;
 import skylight1.sevenwonders.services.SoundTracks;
 import skylight1.sevenwonders.view.SevenWondersGLRenderer;
 import android.os.Handler;
-import android.os.Message;
 
 final class CoinCollisionAction implements CollisionAction {
 	@Override
 	public void collision(Handler aUiHandler, SevenWondersGLRenderer aSevenWondersGLRenderer) {
-		// TODO add a different sound here
-		SoundTracks.getInstance().play(SoundTracks.SPELL);
+		SoundTracks.getInstance().play(SoundTracks.COIN);
 
 		aSevenWondersGLRenderer.getGameState().incrementNumberofCoinsCollected();
 	}
