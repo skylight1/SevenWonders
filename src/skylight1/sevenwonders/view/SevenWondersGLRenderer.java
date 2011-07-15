94c683d107b99b6d82ac80ef54addd1ab35eacb0
lnanek <lnanek@gmail.com>
2011-07-15 00:41:05 +0000

    Fix skybox cutting off distant objects.

--- src/skylight1/sevenwonders/view/SevenWondersGLRenderer.java
+++ src/skylight1/sevenwonders/view/SevenWondersGLRenderer.java
@@ -381,7 +381,7 @@ public class SevenWondersGLRenderer implements Renderer {
 		aGl.glMatrixMode(GL10.GL_PROJECTION);
 		aGl.glLoadIdentity();
 		aGl.glViewport(0, 0, aW, aH);
-		GLU.gluPerspective(aGl, 45, (float) aW / (float) aH, 0.1f, 5000f);
+		GLU.gluPerspective(aGl, 45, (float) aW / (float) aH, 0.1f, 3000.0f);
 
 		aGl.glMatrixMode(GL10.GL_MODELVIEW);
 		
