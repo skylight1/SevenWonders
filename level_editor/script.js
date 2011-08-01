a861d697dce2968d8ee2e1efa2798f122353e521
lnanek <lnanek@gmail.com>
2011-08-01 20:05:33 +0000

    Start dividing canvas up into palette and level area.

--- level_editor/script.js
+++ level_editor/script.js
@@ -1,5 +1,16 @@
 
+// Objects shown in the game for this level.
 var gameObjects;
+
+// Objects offered in the palette, they can be dragged on to the level to add a game object.
+var paletteObjects;
+
+var PALETTE_OBJECT_NAMES = [
+                      "pyramid",
+                      "spell",
+                      "sphinx",
+                      "hazard",
+];
 
 var background;
 var canvas;
@@ -34,8 +45,15 @@ function handleLoad() {
 	}
 		  
 	gameObjects = new Array();
-	gameObjects[0] = new Sprite("test1", canvas.width / 2, canvas.height / 2);
-	gameObjects[1] = new Sprite("test2", canvas.width / 2, 0);
+	gameObjects[0] = new Sprite("sphinx", canvas.width / 2, canvas.height / 2);
+	gameObjects[1] = new Sprite("pyramid", canvas.width / 2, 0);
+	
+	paletteObjects = new Array();
+	var paletteIndex;
+	for( var i in PALETTE_OBJECT_NAMES ) {
+		paletteObjects[paletteIndex++] = new Sprite(
+				PALETTE_OBJECT_NAMES[i], 80, 25 + 80 * paletteIndex);
+	}
 		
 	draw();
 }
@@ -44,11 +62,40 @@ function draw() {
 		
 	ctx.drawImage(background, 0, 0);
 	
+	//Draw game objects.
 	for( var i in gameObjects ) {
 		var gameObject = gameObjects[i];
 		ctx.drawImage(gameObject.image, gameObject.left, gameObject.top);
 	}
+	
+	//Draw palette objects.
+	/*
+	for( var i in paletteObjects ) {
+		var paletteObject = paletteObjects[i];
+		ctx.drawImage(paletteObject.image, paletteObject.left, paletteObject.top);
+	}
+	*/	
 
+	//Draw label for the palette area of the editor.
+	ctx.font = "bold x-large sans-serif";
+	ctx.fillStyle = "white";
+	ctx.textAlign = "left";
+	ctx.fillText("Palette", 5, 25);
+
+	//Draw a vertical line separating it.
+	ctx.moveTo(90, 0);
+	ctx.lineTo(90, 512);
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = "#000";
+	ctx.stroke();
+
+	//Draw label for the play area area of the editor.
+	ctx.font = "bold x-large sans-serif";
+	ctx.fillStyle = "white";
+	ctx.textAlign = "left";
+	ctx.fillText("Play Area", 100, 25);
+	
+	
 }
 
 function collisionCheckSprites(a, b) {
