a0f2f3c29a788803269edf9192954768dbc3ace3
dariol <dario@nycjava.net>
2011-08-26 01:04:32 +0000

    update to latest admob, adwhirl

--- SevenWonders/src/skylight1/sevenwonders/Adverts.java
+++ SevenWonders/src/skylight1/sevenwonders/Adverts.java
@@ -6,16 +6,11 @@ import android.content.Context;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-import com.admob.android.ads.AdManager;
 import com.adwhirl.AdWhirlLayout;
 
 public class Adverts {
 	public static void insertAdBanner(Context context, ViewGroup layout) {
        
-	    //for admob: don't show ads in emulator
-	    AdManager.setTestDevices( new String[] { AdManager.TEST_EMULATOR
-	    //,"your_debugging_phone_id_here" // add phone id if debugging on phone
-	    });
 	    String adwhirl_id = Assets.getString("adwhirl_id",context);
 	    if(adwhirl_id!=null && adwhirl_id.length()>0) {
 //TODO: reflect! - and remove line following
