7f3f94a9642451a77a1e2cb23df7a0291373be58
dariol <dario@nycjava.net>
2011-08-30 00:58:13 +0000

    updated adwhirl code

--- src/skylight1/sevenwonders/Adverts.java
+++ src/skylight1/sevenwonders/Adverts.java
@@ -7,17 +7,29 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.adwhirl.AdWhirlLayout;
+import com.adwhirl.AdWhirlLayout.AdWhirlInterface;
+import com.adwhirl.AdWhirlManager;
+import com.adwhirl.AdWhirlTargeting;
 
 public class Adverts {
 	public static void insertAdBanner(Context context, ViewGroup layout) {
        
 	    String adwhirl_id = Assets.getString("adwhirl_id",context);
 	    if(adwhirl_id!=null && adwhirl_id.length()>0) {
-//TODO: reflect! - and remove line following
-	    	AdWhirlLayout adWhirlLayout = new AdWhirlLayout((Activity) context, adwhirl_id);
-//	    	RelativeLayout adWhirlLayout = new RelativeLayout(context); 	
-	    	RelativeLayout.LayoutParams adWhirlLayoutParams = new RelativeLayout.LayoutParams(320, 52);
-	    	layout.addView(adWhirlLayout, adWhirlLayoutParams);
+
+    	   AdWhirlManager.setConfigExpireTimeout(1000 * 60 * 5);	    	   
+    	   AdWhirlTargeting.setTestMode(false);
+ 	
+    	   AdWhirlLayout adWhirlLayout = new AdWhirlLayout((Activity) context, adwhirl_id);
+    	   RelativeLayout.LayoutParams adWhirlLayoutParams = new RelativeLayout.LayoutParams(320, 52);
+    	   layout.addView(adWhirlLayout, adWhirlLayoutParams);
+    	   int diWidth = 320;
+    	   int diHeight = 52;
+    	   int density = (int) context.getResources().getDisplayMetrics().density;    	 
+    	   adWhirlLayout.setAdWhirlInterface((AdWhirlInterface) context);
+    	   adWhirlLayout.setMaxWidth((int)(diWidth * density));
+    	   adWhirlLayout.setMaxHeight((int)(diHeight * density));
+    	   layout.invalidate();	    
 	    }
     }
 }
