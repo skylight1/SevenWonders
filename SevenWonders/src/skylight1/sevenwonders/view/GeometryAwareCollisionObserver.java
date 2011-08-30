6aee26b0bce8bc46530fc0b2a54343910c79e18f
jokr0815 <jokr0815@skylight1.org>
2011-08-30 04:21:08 +0000

    Deleted contents to synch with trunk

--- SevenWonders/src/skylight1/sevenwonders/view/GeometryAwareCollisionObserver.java
+++
@@ -1,8 +0,0 @@
-package skylight1.sevenwonders.view;
-
-import skylight1.opengl.OpenGLGeometry;
-import skylight1.opengl.CollisionDetector.CollisionObserver;
-
-public interface GeometryAwareCollisionObserver extends CollisionObserver {
-	void addGeometry(OpenGLGeometry anOpenGLGeometry, int anAnimationIndex, int aGeometryIndex);
-}
